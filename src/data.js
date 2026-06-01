def3f45cff6cd7ac7ceec8b38de8f2a48253707b
ayomideRussianCode <ayindeayomide7@gmail.com>
2026-06-01 23:54:49 +0100

    working on architecture

---
+++ src/data.js
@@ -0,0 +1,5 @@
+export const roadmap = {
+  frontend: ["HTML", "CSS", "JavaScript", "React", "Tailwind", "Redux", "TypeScript", "Next.js"],
+  backend:["Node.js", "GO", "PostgreSQL", "REST APIS", "MongoDB", "GraphQL"],
+  problemSolving:["Loops", "Functions", "Arrays", "Recursion", "Dynamic Programming"]
+};
